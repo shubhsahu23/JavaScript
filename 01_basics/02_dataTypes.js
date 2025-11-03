f842b190705cd9c0d7ceb582ba0fce8dfd200eca
shubh <148770212+shubhsahu23@users.noreply.github.com>
2025-11-03 09:03:16 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict";//treat all js code as newer version
+
+//alert("hello");//we are using nodejs not browser
+
+console.log(3+3);//code readability should be high
+console.log("shubh");
+
+let name="shubh";
+let age=20;
+let loggedIn=true;
+let state;
+
+// number=> 2 to power 53
+//bigint
+//string =>""
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+//object
+console.log(typeof(undefined));//undefined
+console.log(typeof(null));//object
