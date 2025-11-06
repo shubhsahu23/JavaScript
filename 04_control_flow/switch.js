044e427f54f8cf0fd69b78ed0fff63ca31b50758
shubh <148770212+shubhsahu23@users.noreply.github.com>
2025-11-06 21:18:39 +0530

    control flow code files

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,23 @@
+//switch(key){
+    //case value:
+        //break;
+    //default:
+        //break;
+//}
+
+const month="mar";
+
+switch(month){
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("feburary");
+        break;
+    case "mar":
+        console.log("March");
+        break;
+    default:
+        console.log("Default case");
+        break;
+}
